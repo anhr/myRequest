e5a45bf3eed34ef6a19fc7aa5c3f38c77840c660
Andrej <anhr@mail.ru>
2015-08-28 08:19:38 +0700

    Cosmetic changes

--- myRequest.js
+++ myRequest.js
@@ -1,4 +1,23 @@
-﻿var myRequest = {
+﻿/**
+ * The myRequest is Javascript object of the cross-browser XMLHttpRequest wrapper. Your request code to the server is very simple, if you are using myRequest obect.
+ * Author: Andrej Hristoliubov
+ * email: anhr@mail.ru
+ * About me: https://googledrive.com/host/0B5hS0tFSGjBZfkhKS1VobnFDTkJKR0tVamxadmlvTmItQ2pxVWR0WDZPdHZxM2hzS1J3ejQ/AboutMe/
+ * source: https://github.com/anhr/myRequest
+ * Licences: GPL, The MIT License (MIT)
+ * Copyright: (c) 2015 Andrej Hristoliubov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * Revision:
+ *  2015-08-28, : 
+ *       + Cosmetic changes
+ *
+ */
+ 
+var myRequest = {
 	Create: function(url, data){
 		var req;
 		if(data)
